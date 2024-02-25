dbd5a5a576bec1d41bb06f3c8b4e8c553571e579
divad <divadchigozie@gmail.com>
2024-02-25 17:38:35 +0100

    added swagger @tags

--- src/main/java/com/budgit/security/AuthenticationController.java
+++ src/main/java/com/budgit/security/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.budgit.security;
 
 import com.budgit.dto.Login;
 import com.budgit.hateoas.model.Response;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +14,14 @@ import reactor.core.publisher.Mono;
 public class AuthenticationController {
 
     @PostMapping(path = "login")
+    @Tag(name = "User log-in")
     public Mono<Response> login(@RequestBody Login login) {
         //ToDo
         return null;
     }
 
     @PostMapping(path = "logout")
+    @Tag(name = "User log-out")
     public Mono<Response> logout() {
         //ToDo
         return null;
